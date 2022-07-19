99b2ef72da773f7aab87941c690013ec9b0cac4e
John Mello <johncharlesmello@gmail.com>
2022-07-19 19:12:30 -0300

    create home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,9 @@
-
-import Navbar from '../components/organisms/Navbar'
 import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Navbar />
+    <div>
+      Home
     </div>
   )
 }
