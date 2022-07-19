054a627a057efd1dce643bf6d077526cfde41ae2
John Mello <johncharlesmello@gmail.com>
2022-07-19 18:59:18 -0300

    add sass

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+
+import Navbar from '../components/organisms/Navbar'
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+    </div>
+  )
+}
