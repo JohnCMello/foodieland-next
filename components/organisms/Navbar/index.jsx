33b72b6fa6e81028fd10d4aa991a2fed5ee071ad
John Mello <johncharlesmello@gmail.com>
2022-07-21 17:50:57 -0300

    add navbar

--- components/organisms/Navbar/index.jsx
+++ components/organisms/Navbar/index.jsx
@@ -1,6 +1,20 @@
+import useWindowWidth from '../../../hooks/usePageWidth'
+import Logo from '../../atoms/FoodielandLogo'
+
+import NavigationMenu from '../../molecules/NavigationMenu'
+import SocialsMenu from '../../molecules/SocialsMenu'
+
+import styles from './styles.module.scss'
+
 const Navbar = () => {
+  const { isMobile } = useWindowWidth()
+
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+      <Logo />
+      <NavigationMenu isMobile={isMobile} />
+      {isMobile ? null : <SocialsMenu />}
+    </nav>
   )
 }
 export default Navbar
