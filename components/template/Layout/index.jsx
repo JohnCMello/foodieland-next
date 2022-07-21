5e7e4dce1893c30ee3ed77902c0f4954cdb3d8c4
John Mello <johncharlesmello@gmail.com>
2022-07-21 17:49:31 -0300

    add layout

--- components/template/Layout/index.jsx
+++ components/template/Layout/index.jsx
@@ -2,6 +2,8 @@ import { Fragment } from 'react'
 import Footer from '../../organisms/Footer'
 import Navbar from '../../organisms/Navbar'
 
+import styles from './styles.module.scss'
+
 const Layout = ({ children }) => {
   return (
     <Fragment>
