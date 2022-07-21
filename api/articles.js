e77ee0c7d028590f36895f22e6a0781a2f476d18
John Mello <johncharlesmello@gmail.com>
2022-07-21 18:17:46 -0300

    add articles file

---
+++ api/articles.js
@@ -0,0 +1,340 @@
+import picture1 from '/public/imgs/chicken-ramen-soup.png';
+import picture2 from '/public/imgs/mixed-tropical-fruit-salad.png';
+import picture3 from '/public/imgs/full-guide-to-become-a-chef.png'
+import picture4 from '/public/imgs/vegetarian-lasagna.png';
+import picture5 from '/public/imgs/plantain-and-pinto-stew-aji-verde.png';
+import picture6 from '/public/imgs/hiring-communication-assistant.png';
+
+import user1 from '/public/imgs/wade-warren.png';
+import user2 from '/public/imgs/robert-fox.png';
+import user3 from '/public/imgs/dianne-russel.png';
+import user4 from '/public/imgs/leslie-alexander.png';
+import user5 from '/public/imgs/courtney-henry.png';
+import user6 from '/public/imgs/albert-flores.png';
+
+export const articles = [
+  {
+    id: '1',
+    image: picture1,
+    title: 'Crochet Project for Noodle Lovers',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user1,
+    userName: 'Wade Warren',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '2',
+    image: picture2,
+    title: '10 Vegetarian Recipes To Eat This Month ',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user2,
+    userName: 'Robert Fox',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '3',
+    image: picture3,
+    title: 'Full Guide to Becoming a Professional Chef',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user3,
+    userName: 'Dianne Russel',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '4',
+    image: picture4,
+    title: 'Simple & Delicious Vegetarian Lasagna',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user4,
+    userName: 'Leslie Alexander',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '5',
+    image: picture5,
+    title: 'Platain and Pinto Stew with Aji Verde',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '6',
+    image: picture6,
+    title: 'We\'re Hiring a Communications Assistant',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '7',
+    image: picture6,
+    title: 'Microwave Popcorn',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user1,
+    userName: 'Wade Warren',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '8',
+    image: picture5,
+    title: 'Two-and-a-half-minute Noodles ',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user2,
+    userName: 'Robert Fox',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '9',
+    image: picture4,
+    title: 'Apple Cobbler to Die For',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user3,
+    userName: 'Dianne Russel',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '10',
+    image: picture3,
+    title: 'MEAT Lasagna For Meat Lovers',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user4,
+    userName: 'Leslie Alexander',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '11',
+    image: picture2,
+    title: 'Boring Salad and Boring Sauce',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '12',
+    image: picture1,
+    title: 'Make one-minute rice in 0:55 seconds',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '13',
+    image: picture4,
+    title: 'Cherry lime granola',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user1,
+    userName: 'Wade Warren',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '14',
+    image: picture3,
+    title: 'Vegetable and tofu pad thai ',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user2,
+    userName: 'Robert Fox',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '15',
+    image: picture5,
+    title: 'The Health Benefits of Food',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user3,
+    userName: 'Dianne Russel',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '16',
+    image: picture2,
+    title: 'Oils and Fats: How Food Affects Health',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user4,
+    userName: 'Leslie Alexander',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '17',
+    image: picture6,
+    title: 'Everything is better with bacon',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '18',
+    image: picture1,
+    title: 'Mother’s Day Brunch Ideas',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '19',
+    image: picture6,
+    title: 'Everything is better with bacon',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '20',
+    image: picture1,
+    title: 'Mother’s Day Brunch Ideas',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '21',
+    image: picture5,
+    title: 'Chocolate Indulgences and a Pasta Hack',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '22',
+    image: picture2,
+    title: 'Party time! Sliders and slaw',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '23',
+    image: picture4,
+    title: 'Low-carb Buns and a Delish Greek Burger',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '24',
+    image: picture3,
+    title: 'Prime Day Deals Happy Hour!',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '25',
+    image: picture2,
+    title: 'Everything is better with bacon',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '26',
+    image: picture4,
+    title: 'Backyard Cookout',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '27',
+    image: picture6,
+    title: 'Quickie Meals',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '28',
+    image: picture1,
+    title: 'Summer Superfoods',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '29',
+    image: picture3,
+    title: 'Foods to eat in 2022',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '30',
+    image: picture5,
+    title: 'Budget-friendly bites',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '31',
+    image: picture5,
+    title: 'Easy at-home recipes',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '32',
+    image: picture4,
+    title: 'Quick, Easy Recipe Hacks',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '33',
+    image: picture3,
+    title: 'Simple, Tasty Snacks',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '34',
+    image: picture1,
+    title: 'Burgers and Onion Rings',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '35',
+    image: picture2,
+    title: 'Chocolate Goodies',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publishedAt: '12 November 2021'
+  },
+  {
+    id: '36',
+    image: picture6,
+    title: 'Delish Ways to Sneak in Produce',
+    description: 'Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publishedAt: '12 November 2021'
+  }
+];
